a5f207607efc298dd92a7c8acc4cfc9b4340bfe8
Fabio Ferreira <fabio.ferreira3@outlook.com>
2024-02-26 19:43:46 -0300

    remove border

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -45,8 +45,8 @@ const Testimonials = ({testimonials, duration}: any) => {
             className={`flex-1 border border-gray-800 bg-gray-900 rounded-2xl md:flex-none w-full md:w-1/3 p-4 transition-opacity duration-500 ${fade ? 'opacity-100' : 'opacity-0'}`}
           >
             <div className="flex flex-col text-center h-full justify-center gap-4">
-              <p className="text-lg italic text-gray-100">{`"${testimonial.quote}"`}</p>
-              <p className="flex items-center justify-center gap-4 text-sm mt-2 text-gray-100">
+              <p className="text-lg italic text-gray-300">{`"${testimonial.quote}"`}</p>
+              <p className="flex items-center justify-center gap-4 text-sm mt-2 text-gray-300">
                 <Image style={{objectFit: "cover"}} className="rounded-full h-12 w-12" width={50} height={50} src={testimonial.pic} alt="logo" />
                 {testimonial.author}</p>
             </div>
