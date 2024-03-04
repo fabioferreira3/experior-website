7ffdc60cf1f5a6d5d95f245fb9210962e18fa04f
Fabio Ferreira <fabio.ferreira3@outlook.com>
2024-03-03 21:12:14 -0300

    removed facebook

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,14 +13,14 @@ const Footer = () => {
   return (
     <footer className="md:max-w-6xl m-auto p-8 md:px-12 md:py-4">
       <div className="flex justify-center border-b border-gray-500 pb-8">
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-4">
           <Image
             alt="Experior logo"
             width={180}
             height={50}
             src={"/logo.png"}
           />
-          <div className="flex items-center ml-6 mt-4 gap-4">
+          <div className="flex justify-center items-center gap-4">
             <Link
               target="_blank"
               href="https://linkedin.com/company/experior-ai"
